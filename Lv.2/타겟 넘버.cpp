1a6f406f5554e0b049901776144a6cd152fded41
j-kyung99 <86589951+j-kyung99@users.noreply.github.com>
2022-05-29 16:20:20 +0900

    Create 타겟 넘버.cpp

---
+++ Lv.2/타겟 넘버.cpp
@@ -0,0 +1,18 @@
+#include <string>	
+#include <vector>	
+using namespace std;	
+int answer=0;	
+void get_target(vector<int> numbers, int target, int sum, int idx){	
+    if(idx == numbers.size()){	
+        if(sum == target){	
+            answer++;	
+        }	
+        return;	
+    }	
+    get_target(numbers, target, sum + numbers[idx], idx+1);	
+    get_target(numbers, target, sum - numbers[idx], idx+1);	
+}	
+int solution(vector<int> numbers, int target) {	
+    get_target(numbers, target, 0, 0);	
+    return answer;	
+}
