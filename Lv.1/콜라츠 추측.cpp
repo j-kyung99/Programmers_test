309ddb2ce2e153c880dd3ea75a2608cba542a299
j-kyung99 <86589951+j-kyung99@users.noreply.github.com>
2022-10-20 21:23:13 +0900

    Create 콜라츠 추측.cpp
    int 초과하는 경우를 잊지말자 long!!

---
+++ Lv.1/콜라츠 추측.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num) {
+    int answer = 0;
+    long n = num;
+    while(1){
+        if(n == 1) break;
+        if(n % 2 == 0){
+            n = n / 2;
+            answer++;
+        }
+        else { 
+            n = n * 3 + 1;
+            answer++;
+        }
+        if(answer >= 500) {
+            answer = -1;
+            break;
+        }
+    }
+    return answer;
+}
